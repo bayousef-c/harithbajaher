513e07034d14d88463e8cfdfdcfb21bf1d2a3fce
bayousef-c <hsab7164@gmail.com>
2025-01-14 19:13:13 +0300

    mm
    main

---
+++ hi/studnt.dart
@@ -0,0 +1,9 @@
+main() {
+  String myDetails = '''
+MY NAME : Alharith Abdullah Bajaher.
+registration Number : 52222189.
+Specialixation : Infromation tecnology.
+Level : Level 3;
+''';
+  print(myDetails);
+}
